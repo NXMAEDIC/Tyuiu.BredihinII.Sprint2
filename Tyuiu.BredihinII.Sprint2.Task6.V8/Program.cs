bad07e940d30aaaf932d3c8467408129562401b3
nomadic <nomadic@NOMAD>
2025-10-15 01:53:40 +0500

    2

--- Tyuiu.BredihinII.Sprint2.Task6.V8/Program.cs
+++ Tyuiu.BredihinII.Sprint2.Task6.V8/Program.cs
@@ -6,10 +6,10 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите номер месяца (n): ");
+            Console.Write("Введите номер месяца: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите число месяца (m): ");
+            Console.Write("Введите число месяца: ");
             int m = Convert.ToInt32(Console.ReadLine());
 
             DataService ds = new DataService();
